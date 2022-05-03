34ca42726a87dc5efbcfa7ba52897fb28b90c8d1
Nayan Bhut <nayan.bhut@brainvire.com>
2022-05-03 16:22:25 +0530

    Added ProcFile

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -16,15 +16,30 @@ public func configure(_ app: Application) throws {
 
 
 func setDatabase(_ app: Application) {
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "postgres",
-        password: Environment.get("DATABASE_PASSWORD") ?? "root",
-        database: Environment.get("DATABASE_NAME") ?? "postgres"
-    ), as: .psql)
+
     
+//    do {
+//        try app.databases.use(.postgres(url: "postgres://dqwutrojebehyw:5b349c3105b2c3eda4a8be8f75d11309cce22e816960edb9a25e05e5ec681339@ec2-3-224-164-189.compute-1.amazonaws.com:5432/de7pak0gtktq1p"), as: .psql)
+//    }catch {
+//        print("DataBase not connected ",error.localizedDescription)
+//    }
     
+    if let url = Environment.get("DATABASE_URL") {
+        do {
+            try app.databases.use(.postgres(url: url), as: .psql)
+        }catch {
+            print("DataBase not connected ",error.localizedDescription)
+        }
+    }else {
+        
+        app.databases.use(.postgres(
+            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+            username: Environment.get("DATABASE_USERNAME") ?? "postgres",
+            password: Environment.get("DATABASE_PASSWORD") ?? "root",
+            database: Environment.get("DATABASE_NAME") ?? "postgres"
+        ), as: .psql)
+    }
 }
 
 
