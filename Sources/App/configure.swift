f00cc074de74c5eee1ee991e142e8e216bdb01bb
Nayan Bhut <nayan.bhut@brainvire.com>
2022-05-03 20:06:50 +0530

    Database config Changes

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -16,15 +16,21 @@ public func configure(_ app: Application) throws {
 
 
 func setDatabase(_ app: Application) {
-    let tlsConfiguration = TLSConfiguration.forClient(certificateVerification: .none)
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "postgres",
-        password: Environment.get("DATABASE_PASSWORD") ?? "root",
-        database: Environment.get("DATABASE_NAME") ?? "postgres",
-        tlsConfiguration: tlsConfiguration
-    ), as: .psql)
+    if let url = Environment.get("DATABASE_URL") {
+        var postgresConfiguration = PostgresConfiguration(url: url)!
+        postgresConfiguration.tlsConfiguration = .makeClientConfiguration()
+        postgresConfiguration.tlsConfiguration?.certificateVerification = .none
+        app.databases.use(.postgres(configuration: postgresConfiguration), as: .psql)
+    }else {
+        
+        app.databases.use(.postgres(
+            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+            username: Environment.get("DATABASE_USERNAME") ?? "postgres",
+            password: Environment.get("DATABASE_PASSWORD") ?? "root",
+            database: Environment.get("DATABASE_NAME") ?? "postgres"
+        ), as: .psql)
+    }
 }
 
 
